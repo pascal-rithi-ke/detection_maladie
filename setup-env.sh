b3e67907f8e0eabb23992c2bc12a58c23c2141f1
Pascal-ke <pascal.rithi@gmail.com>
2024-06-06 20:06:24 +0200

    script setup env python

---
+++ setup-env.sh
@@ -0,0 +1,8 @@
+# Create python environment
+python -m venv venv
+# Activate python environment
+source venv/Scripts/activate
+# Install python packages
+pip install -r requirements.txt
+# Deactivate python environment
+deactivate
